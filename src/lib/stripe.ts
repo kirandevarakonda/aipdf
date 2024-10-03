318c772c272bd6dec0a220220428ee366a252d29
kirandevarakonda <73365095+kirandevarakonda@users.noreply.github.com>
2024-10-03 16:12:32 +0530

    Update stripe.ts

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -7,4 +7,4 @@
 
 import { loadStripe } from '@stripe/stripe-js';
 
-const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+// const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
