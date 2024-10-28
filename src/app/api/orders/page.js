eca733b24820341819546bc28bb3605a3f8505e9
kirandevarakonda <devarakondasriramkiran@gmail.com>
2024-10-28 22:56:52 +0530

    content commit

--- src/app/api/orders/page.js
+++ src/app/api/orders/page.js
@@ -22,57 +22,57 @@
 // </script>
 
 
-import { useEffect } from "react";
-import {
-	PayPalScriptProvider,
-	PayPalButtons,
-	usePayPalScriptReducer
-} from "@paypal/react-paypal-js";
+// import { useEffect } from "react";
+// import {
+// 	PayPalScriptProvider,
+// 	PayPalButtons,
+// 	usePayPalScriptReducer
+// } from "@paypal/react-paypal-js";
 
-const ButtonWrapper = ({ type }) => {
-	const [{ options }, dispatch] = usePayPalScriptReducer();
+// const ButtonWrapper = ({ type }) => {
+// 	const [{ options }, dispatch] = usePayPalScriptReducer();
 
-	useEffect(() => {
-        dispatch({
-            type: "resetOptions",
-            value: {
-                ...options,
-                intent: "subscription",
-            },
-        });
-    }, [type]);
+// 	useEffect(() => {
+//         dispatch({
+//             type: "resetOptions",
+//             value: {
+//                 ...options,
+//                 intent: "subscription",
+//             },
+//         });
+//     }, [type]);
 
-	return (<PayPalButtons
-		createSubscription={(data, actions) => {
-			return actions.subscription
-				.create({
-					plan_id: "P-3RX065706M3469222L5IFM4I",
-				})
-				.then((orderId) => {
-					// Your code here after create the order
-					return orderId;
-				});
-		}}
-		style={{
-			label: "subscribe",
-		}}
-	/>);
-}
+// 	return (<PayPalButtons
+// 		createSubscription={(data, actions) => {
+// 			return actions.subscription
+// 				.create({
+// 					plan_id: "P-3RX065706M3469222L5IFM4I",
+// 				})
+// 				.then((orderId) => {
+// 					// Your code here after create the order
+// 					return orderId;
+// 				});
+// 		}}
+// 		style={{
+// 			label: "subscribe",
+// 		}}
+// 	/>);
+// }
 
-export default function App() {
-	return (
-		<PayPalScriptProvider
-			options={{
-				clientId: "test",
-				components: "buttons",
-				intent: "subscription",
-				vault: true,
-			}}
-		>
-			<ButtonWrapper type="subscription" />
-		</PayPalScriptProvider>
-	);
-}
+// export default function App() {
+// 	return (
+// 		<PayPalScriptProvider
+// 			options={{
+// 				clientId: "test",
+// 				components: "buttons",
+// 				intent: "subscription",
+// 				vault: true,
+// 			}}
+// 		>
+// 			<ButtonWrapper type="subscription" />
+// 		</PayPalScriptProvider>
+// 	);
+// }
 
 // import Head from 'next/head';
 
